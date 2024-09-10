3d3543ce55775c9cd1dacb6726f75aff793c6f8f
adelelakour <adel.elakour@gmail.com>
2024-09-10 22:06:40 +0200

    first commit

---
+++ src/main.cpp
@@ -0,0 +1,17 @@
+#include "ncurses_display.h"
+#include "system.h"
+#include <iostream>
+int main() {
+
+  std::cout << "Helloooooooooooooooooooo \n";
+
+
+  /*
+  System system;
+  NCursesDisplay::Display(system);
+  */
+
+
+
+
+}
